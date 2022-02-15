4d7256bf1fbe3f1e7b148b458072eeccd8f8bedf
Jonas Kristoffersen <jo_nas@live.no>
2022-02-15 15:56:44 +0100

    feat: added damage and attackSpeed members to Weapon and made constructor for Weapon

--- src/item/Weapon.java
+++ src/item/Weapon.java
@@ -2,6 +2,22 @@ package item;
 
 public class Weapon extends Item {
     private WeaponType weaponType;
+    private float damage;
+    private float attackSpeed;
+
+    public Weapon(String name, int levelRequirement, Slot slot, float damage, float attackSpeed) {
+        super(name, levelRequirement, slot);
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public float getDamage() {
+        return damage;
+    }
+
+    public float getAttackSpeed() {
+        return attackSpeed;
+    }
 
     public WeaponType getWeaponType() {
         return weaponType;
