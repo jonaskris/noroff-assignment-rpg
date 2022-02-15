9b3593ca9f6ff17c28478edb1a8167a603a4592f
Jonas Kristoffersen <jo_nas@live.no>
2022-02-15 16:11:40 +0100

    feat: added InvalidWeaponException and InvalidArmorException

---
+++ src/item/InvalidArmorException.java
@@ -0,0 +1,7 @@
+package item;
+
+public class InvalidArmorException extends Exception {
+    public InvalidArmorException(String message) {
+        super(message);
+    }
+}
---
+++ src/item/InvalidWeaponException.java
@@ -0,0 +1,7 @@
+package item;
+
+public class InvalidWeaponException extends Exception {
+    public InvalidWeaponException(String message) {
+        super(message);
+    }
+}
