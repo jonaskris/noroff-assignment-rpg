a31d11b48fcd3ff80563cc563963ef371e09790d
Jonas Kristoffersen <jo_nas@live.no>
2022-02-16 21:35:16 +0100

    chore: defined a Warrior Character with items equipped to show off character sheet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import attribute.Attributes;
+import character.Warrior;
+import item.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Warrior warrior = new Warrior("Muradin Bronzebeard");
+        for(int i = 0; i < 20; i++)
+            warrior.levelUp();
+
+        Weapon weapon = new Weapon("Legendary Swift Axe", 15, WeaponType.Axe, 45, 1.4f);
+        Armor head = new Armor("Fierce Pot With Handle", 9, Slot.Head, ArmorType.Plate, new Attributes(5, 0, 0));
+        Armor body = new Armor("Sturdy Chestplate", 8, Slot.Body, ArmorType.Plate, new Attributes(8, 2, 1));
+        Armor legs = new Armor("Mythic Mail Leggings", 13, Slot.Legs, ArmorType.Mail, new Attributes(4, 4, 2));
+
+        try {
+            warrior.equip(weapon);
+            warrior.equip(head);
+            warrior.equip(body);
+            warrior.equip(legs);
+        } catch(Exception e) {
+            System.out.println("Failed to equip items. Exception message: " + e.getMessage());
+        }
+
+        System.out.println(warrior);
+
     }
 }
