83fb2a1763864ada91ccaa54a22138bc1c9bf5af
Jonas Kristoffersen <jo_nas@live.no>
2022-02-16 19:54:35 +0100

    fix: Weapon member weaponType was never assigned. WeaponType is now set as constructor parameter

--- src/item/Weapon.java
+++ src/item/Weapon.java
@@ -5,8 +5,9 @@ public class Weapon extends Item {
     private float damage;
     private float attackSpeed;
 
-    public Weapon(String name, int levelRequirement, float damage, float attackSpeed) {
+    public Weapon(String name, int levelRequirement, WeaponType weaponType, float damage, float attackSpeed) {
         super(name, levelRequirement, Slot.Weapon);
+        this.weaponType = weaponType;
         this.damage = damage;
         this.attackSpeed = attackSpeed;
     }
